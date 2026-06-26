ba493121d55a4dd7469c85d6979dd1f795e19d34
GustavoBenAbraham <gustavohabran@gmail.com>
2026-06-26 15:35:39 -0300

    Commit

---
+++ CursoJavaBasico/src/Aula13Exercicio02/numeroInformado.java
@@ -0,0 +1,26 @@
+package Aula13Exercicio02;
+
+import java.util.Scanner;
+
+public class numeroInformado {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		 
+		System.out.println("Digite um número: ");
+		int número = scan.nextInt();
+		
+		
+		System.out.println("O número informado foi: " + número);
+		
+		scan.close();
+			
+		
+		
+			
+		
+
+	}
+
+}
