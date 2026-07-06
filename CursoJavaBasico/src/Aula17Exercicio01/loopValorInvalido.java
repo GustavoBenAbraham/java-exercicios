728f887bb4ba26299f90bfe793cb9e5e205840d0
GustavoBenAbraham <gustavohabran@gmail.com>
2026-07-06 17:22:05 -0300

    Aula 17 - Exercicio 01 - Loops

---
+++ CursoJavaBasico/src/Aula17Exercicio01/loopValorInvalido.java
@@ -0,0 +1,28 @@
+package Aula17Exercicio01;
+
+import java.util.Scanner;
+
+public class loopValorInvalido {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		double nota;
+		do {
+		    System.out.println("Digite uma nota entre [0-10]: ");
+		    nota = scan.nextDouble();
+		    
+		    if (nota < 0 || nota > 10) {
+		        System.out.println("Valor inválido!");
+		    }
+		} while (nota < 0 || nota > 10);
+
+		System.out.println("Nota válida recebida: " + nota);
+		
+		scan.close();
+		return;
+	
+
+	}
+
+}
